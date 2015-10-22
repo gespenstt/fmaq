fd4e2a66fe016a4e718be402d2a4805e1f3ada20
Jaime Vasquez <rodrigoxv@gmail.com>
2015-10-22 01:50:03 -0300

    fix modales

--- php/apps/backend/modules/componentes/templates/_modales.php
+++ php/apps/backend/modules/componentes/templates/_modales.php
@@ -1,212 +1,212 @@
 
 
-					<div id="modal-campo" class="modal fade" tabindex="-1" role="dialog">
-						<div class="modal-dialog">
-							<div class="modal-content">
+    <div id="modal-campo" class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog">
+                    <div class="modal-content">
 
-							<div class="modal-header">
-								<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-								<h4 class="modal-title">Campo</h4>
-							</div>
-							<div class="modal-body">
-								
-								<h4>Agregar</h4>
-								<br>
-								
-								<div>
+                    <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                            <h4 class="modal-title">Campo</h4>
+                    </div>
+                    <div class="modal-body">
 
-									<form action="/" role="form" class="form-horizontal col-md-12">
+                            <h4>Agregar</h4>
+                            <br>
 
-										<div class="form-group">
-											<label class="col-md-4">Nombre</label>
-											<div class="col-md-8">
-												<input type="text" name="input1" placeholder="Nombre" required="required" value="" class="form-control" />
-											</div>
-										</div> <!-- /.form-group -->
+                            <div>
 
-										<div class="form-group">
-											<label class="col-md-4">Direccion</label>
-											<div class="col-md-8">
-												<input type="text" name="input1" placeholder="Apellido" required="required" value="" class="form-control" />
-											</div>
-										</div> <!-- /.form-group -->
+                                    <form action="/" role="form" class="form-horizontal col-md-12">
 
-										<div class="form-group">
-											<label class="col-md-4">Contacto</label>
-											<div class="col-md-8">
-												<input type="email" name="input1" placeholder="contacto@dominio.tld" required="required" value="" class="form-control" />
-											</div>
-										</div> <!-- /.form-group -->
+                                            <div class="form-group">
+                                                    <label class="col-md-4">Nombre</label>
+                                                    <div class="col-md-8">
+                                                            <input type="text" name="input1" placeholder="Nombre" required="required" value="" class="form-control" />
+                                                    </div>
+                                            </div> <!-- /.form-group -->
 
-										<hr />
+                                            <div class="form-group">
+                                                    <label class="col-md-4">Direccion</label>
+                                                    <div class="col-md-8">
+                                                            <input type="text" name="input1" placeholder="Apellido" required="required" value="" class="form-control" />
+                                                    </div>
+                                            </div> <!-- /.form-group -->
 
-										
-										<div class="form-group">
+                                            <div class="form-group">
+                                                    <label class="col-md-4">Contacto</label>
+                                                    <div class="col-md-8">
+                                                            <input type="email" name="input1" placeholder="contacto@dominio.tld" required="required" value="" class="form-control" />
+                                                    </div>
+                                            </div> <!-- /.form-group -->
 
-											<div class="col-md-offset-4 col-md-8">
-
-												<button type="submit" class="btn btn-success">Crear</button>
-
-												<button class="btn btn-default">Cancelar</button>
-											</div>
-
-										</div> <!-- /.form-group -->
-
-									</form>
-
-								</div>
-
-							</div>
-
-							</div>
-						</div>
-					</div>
+                                            <hr />
 
 
-					<div id="modal-potrero" class="modal fade" tabindex="-1" role="dialog">
-						<div class="modal-dialog">
-							<div class="modal-content">
+                                            <div class="form-group">
 
-							<div class="modal-header">
-								<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-								<h4 class="modal-title">Potrero</h4>
-							</div>
-							<div class="modal-body">
+                                                    <div class="col-md-offset-4 col-md-8">
 
-								<p>
-								
-								<h4>Agregar</h4>
-								<br>
+                                                            <button type="submit" class="btn btn-success">Crear</button>
 
-									<form action="/" role="form" class="form-horizontal  col-md-12">
+                                                            <button class="btn btn-default">Cancelar</button>
+                                                    </div>
 
-										<div class="form-group">
-											<label class="col-md-4">Nombre</label>
-											<div class="col-md-8">
-												<input type="text" name="input1" placeholder="Nombre" required="required" value="" class="form-control" />
-											</div>
-										</div> <!-- /.form-group -->
+                                            </div> <!-- /.form-group -->
 
-										<div class="form-group">
-											<label class="col-md-4">Ubicación</label>
-											<div class="col-md-8">
-												<input type="text" name="input1" placeholder="Ubicación" required="required" value="" class="form-control" />
-											</div>
-										</div> <!-- /.form-group -->
+                                    </form>
 
-										<div class="form-group">
-											<label class="col-md-4">Cantidad de hectareas</label>
-											<div class="col-md-8">
-												<input type="number" name="input1" placeholder="0" required="required" value="" class="form-control" />
-											</div>
-										</div> <!-- /.form-group -->
+                            </div>
 
-										<hr />
-										
-										<div class="form-group">
+                    </div>
 
-											<div class="col-md-offset-4 col-md-8">
-
-												<button type="submit" class="btn btn-success">Crear</button>
-
-												<button class="btn btn-default">Cancelar</button>
-											</div>
-
-										</div> <!-- /.form-group -->
-
-									</form>
-
-								</p>
-
-							</div>
+                    </div>
+            </div>
+    </div>
 
 
-							</div>
-						</div>
-					</div>
+    <div id="modal-potrero" class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog">
+                    <div class="modal-content">
+
+                    <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                            <h4 class="modal-title">Potrero</h4>
+                    </div>
+                    <div class="modal-body">
+
+                            <p>
+
+                            <h4>Agregar</h4>
+                            <br>
+
+                                    <form action="/" role="form" class="form-horizontal  col-md-12">
+
+                                            <div class="form-group">
+                                                    <label class="col-md-4">Nombre</label>
+                                                    <div class="col-md-8">
+                                                            <input type="text" name="input1" placeholder="Nombre" required="required" value="" class="form-control" />
+                                                    </div>
+                                            </div> <!-- /.form-group -->
+
+                                            <div class="form-group">
+                                                    <label class="col-md-4">Ubicación</label>
+                                                    <div class="col-md-8">
+                                                            <input type="text" name="input1" placeholder="Ubicación" required="required" value="" class="form-control" />
+                                                    </div>
+                                            </div> <!-- /.form-group -->
+
+                                            <div class="form-group">
+                                                    <label class="col-md-4">Cantidad de hectareas</label>
+                                                    <div class="col-md-8">
+                                                            <input type="number" name="input1" placeholder="0" required="required" value="" class="form-control" />
+                                                    </div>
+                                            </div> <!-- /.form-group -->
+
+                                            <hr />
+
+                                            <div class="form-group">
+
+                                                    <div class="col-md-offset-4 col-md-8">
+
+                                                            <button type="submit" class="btn btn-success">Crear</button>
+
+                                                            <button class="btn btn-default">Cancelar</button>
+                                                    </div>
+
+                                            </div> <!-- /.form-group -->
+
+                                    </form>
+
+                            </p>
+
+                    </div>
 
 
-					<div id="modal-cv" class="modal fade" tabindex="-1" role="dialog">
-						<div class="modal-dialog">
-							<div class="modal-content">
-
-							<div class="modal-header">
-								<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-								<h4 class="modal-title">CV</h4>
-							</div>
-							<div class="modal-body">
-
-							<div class="col-md-12 form-horizontal ">
-
-								<div class="form-group">
-									<label class="col-md-4">Nombre</label>
-									<div id="modal-cv-nombre" class="col-md-8">
-									</div>
-								</div> <!-- /.form-group -->
-
-								<div class="form-group">
-									<label class="col-md-4">Rut</label>
-									<div id="modal-cv-rut" class="col-md-8">
-									</div>
-								</div> <!-- /.form-group -->
-
-								<div class="form-group">
-									<label class="col-md-4">Telefono</label>
-									<div id="modal-cv-telefono" class="col-md-8">
-									</div>
-								</div> <!-- /.form-group -->
-
-								<div class="form-group">
-									<label class="col-md-4">Carta presentación</label>
-									<div id="modal-cv-carta" class="col-md-8">
-									</div>
-								</div> <!-- /.form-group -->
-
-								<hr />
-
-								<div class="form-group">
-									<label class="col-md-4">Adjunto</label>
-									<div class="col-md-8">
-                                                                            <a target="_blank" id="modal-cv-archivo"><i class="icon-download-alt"></i> Descargar</a> 
-									</div>
-								</div> <!-- /.form-group -->
-
-							</div>
-
-							</div>
+                    </div>
+            </div>
+    </div>
 
 
-							</div>
-						</div>
-					</div>
+    <div id="modal-cv" class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog">
+                    <div class="modal-content">
+
+                    <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                            <h4 class="modal-title">CV</h4>
+                    </div>
+                    <div class="modal-body">
+
+                    <div class="col-md-12 form-horizontal ">
+
+                            <div class="form-group">
+                                    <label class="col-md-4">Nombre</label>
+                                    <div id="modal-cv-nombre" class="col-md-8">
+                                    </div>
+                            </div> <!-- /.form-group -->
+
+                            <div class="form-group">
+                                    <label class="col-md-4">Rut</label>
+                                    <div id="modal-cv-rut" class="col-md-8">
+                                    </div>
+                            </div> <!-- /.form-group -->
+
+                            <div class="form-group">
+                                    <label class="col-md-4">Telefono</label>
+                                    <div id="modal-cv-telefono" class="col-md-8">
+                                    </div>
+                            </div> <!-- /.form-group -->
+
+                            <div class="form-group">
+                                    <label class="col-md-4">Carta presentación</label>
+                                    <div id="modal-cv-carta" class="col-md-8">
+                                    </div>
+                            </div> <!-- /.form-group -->
+
+                            <hr />
+
+                            <div class="form-group">
+                                    <label class="col-md-4">Adjunto</label>
+                                    <div class="col-md-8">
+                                        <a target="_blank" id="modal-cv-archivo"><i class="icon-download-alt"></i> Descargar</a> 
+                                    </div>
+                            </div> <!-- /.form-group -->
+
+                    </div>
+
+                    </div>
 
 
-					<div id="modal-imagen" class="modal fade" tabindex="-1" role="dialog">
-						<div class="modal-dialog">
-							<div class="modal-content">
-
-							<div class="modal-header">
-								<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-								<h4 class="modal-title">Imagen</h4>
-							</div>
-							<div class="modal-body">
-
-							<div class="col-md-12 form-horizontal ">
-
-								<img id="modal-imagen-img-src" width="100%" src="<?php echo public_path("img/gallery/lr1_large.png"); ?>" />
-
-								<hr />
-
-								<form action="<?php echo url_for("maquinaria/eliminarimagen"); ?>" method="post">
-                                                                    <input type="hidden" id="modal-imagen-id" name="mfo_id" value="" />
-								<input type="submit" value="Eliminar imagen" class="btn btn-danger" />
-								</form>
-
-							</div>
-
-							</div>
+                    </div>
+            </div>
+    </div>
 
 
-							</div>
-						</div>
-					</div>
+    <div id="modal-imagen" class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog">
+                    <div class="modal-content">
+
+                    <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                            <h4 class="modal-title">Imagen</h4>
+                    </div>
+                    <div class="modal-body">
+
+                    <div class="col-md-12 form-horizontal ">
+
+                            <img id="modal-imagen-img-src" width="100%" src="<?php echo public_path("img/gallery/lr1_large.png"); ?>" />
+
+                            <hr />
+
+                            <form action="<?php echo url_for("maquinaria/eliminarimagen"); ?>" method="post">
+                                <input type="hidden" id="modal-imagen-id" name="mfo_id" value="" />
+                            <input type="submit" value="Eliminar imagen" class="btn btn-danger" />
+                            </form>
+
+                    </div>
+
+                    </div>
+
+
+                    </div>
+            </div>
+    </div>
