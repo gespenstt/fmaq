b0c5697c071ce11aad43ea83cfcc085748d49f90
Jaime Vasquez <rodrigoxv@gmail.com>
2015-10-26 15:07:41 -0300

    update home

--- php/apps/frontend/modules/home/templates/indexSuccess.php
+++ php/apps/frontend/modules/home/templates/indexSuccess.php
@@ -4,7 +4,8 @@
 
 			<div class="swiper-container swiper-parent">
 				<div class="swiper-wrapper">
-					<div class="swiper-slide" style="background-image: url('uploads/slide-1.jpg'); background-position: center top;">
+                                        <?php for($a=0;$a<=3;$a++){?>
+					<div class="swiper-slide" style="background-image: url('<?php echo public_path("uploads/slide-1.jpg");?>'); background-position: center top;">
 						<div class="container clearfix">
 							<div class="slider-caption">
 								<h2>Maquinarias Futamaq</h2>
@@ -12,24 +13,7 @@
 							</div>
 						</div>
 					</div>
-
-					<div class="swiper-slide" style="background-image: url('uploads/slide-2.jpg'); background-position: center top;">
-						<div class="container clearfix">
-							<div  class="slider-caption">
-								<h2>Control Georeferenciado</h2>
-								<p>Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
-							</div>
-						</div>
-					</div>
-
-					<div class="swiper-slide" style="background-image: url('uploads/slide-3.jpg'); background-position: center bottom;">
-						<div class="container clearfix">
-							<div  class="slider-caption">
-								<h2>Agricultura de precisión</h2>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
-							</div>
-						</div>
-					</div>
+                                        <?php } ?>
 				</div>
 				<div id="slider-arrow-left"><i class="icon-angle-left"></i></div>
 				<div id="slider-arrow-right"><i class="icon-angle-right"></i></div>
@@ -47,16 +31,18 @@
 					<div class="container clearfix">
 						<h3 style="letter-spacing: 2px;">¿Estás interesado en trabajar con nosotros?</h3>
 						<span>Contáctanos completando el siguiente formulario y se parte de nuestro equipo</span>
-						<a href="#" class="button button-large button-border button-rounded">Contactar aquí</a>
+						<a href="<?php echo url_for("contacto/index");?>" class="button button-large button-border button-rounded">Contactar aquí</a>
 					</div>
 				</div>
 
 				<div class="container bottommargin-lg clearfix">
+                                    
+                                    <?php for($a=1;$a<=3;$a++){?>
 
-					<div class="col_one_third nobottommargin">
+                                        <div class="col_one_third nobottommargin <?php if($a==3){ echo "col_last"; }?>">
 						<div class="feature-box media-box">
 							<div class="fbox-media">
-								<img style="border-radius: 2px;" src="uploads/400x250-servicios-home-1.jpg" alt="">
+								<img style="border-radius: 2px;" src="<?php echo public_path("uploads/400x250-servicios-home-1.jpg");?>" alt="">
 							</div>
 							<div class="fbox-desc">
 								<h3>Profesionales expertos.<span class="subtitle">Contamos con personal especializado.</span></h3>
@@ -64,30 +50,7 @@
 							</div>
 						</div>
 					</div>
-
-					<div class="col_one_third nobottommargin">
-						<div class="feature-box media-box">
-							<div class="fbox-media">
-								<img style="border-radius: 2px;" src="uploads/400x250-servicios-home-2.jpg" alt="">
-							</div>
-							<div class="fbox-desc">
-								<h3>Servicios especializados.<span class="subtitle">Ofrecemo los mejores servicios agrícolas.</span></h3>
-								<p>Porro repellat vero sapiente amet vitae quibusdam necessitatibus consectetur, labore totam. Accusamus perspiciatis asperiores labore esse.</p>
-							</div>
-						</div>
-					</div>
-
-					<div class="col_one_third nobottommargin col_last">
-						<div class="feature-box media-box">
-							<div class="fbox-media">
-								<img style="border-radius: 2px;" src="uploads/400x250-servicios-home-3.jpg" alt="">
-							</div>
-							<div class="fbox-desc">
-								<h3>Tecnología de punta.<span class="subtitle">A la vanguardia en tecnología agrícola.</span></h3>
-								<p>Quos, non, esse eligendi ab accusantium voluptatem. Maxime eligendi beatae, atque tempora ullam. Vitae delectus quia, consequuntur rerum quo.</p>
-							</div>
-						</div>
-					</div>
+                                    <?php } ?>
 
 				</div>
 
@@ -99,42 +62,16 @@
 
 					<div class="clear-bottommargin">
 						<div class="row common-height clearfix">
+                                                    
+                                                    <?php for($a=1;$a<=6;$a++){?>
 							<div class="col-md-4 col-sm-6 bottommargin">
 								<div class="feature-box fbox-plain">
-									<h3>Algo que hacemos 1</h3>
+									<h3>Algo que hacemos <?php echo $a; ?></h3>
 									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem minima, facere distinctio placeat.</p>
 								</div>
 							</div>
-							<div class="col-md-4 col-sm-6 bottommargin">
-								<div class="feature-box fbox-plain">
-									<h3>Algo que hacemos 2</h3>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem minima, facere distinctio placeat.</p>
-								</div>
-							</div>
-							<div class="col-md-4 col-sm-6 bottommargin">
-								<div class="feature-box fbox-plain">
-									<h3>Algo que hacemos 3</h3>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem minima, facere distinctio placeat.</p>
-								</div>
-							</div>
-							<div class="col-md-4 col-sm-6 bottommargin">
-								<div class="feature-box fbox-plain">
-									<h3>Algo que hacemos 4</h3>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem minima, facere distinctio placeat.</p>
-								</div>
-							</div>
-							<div class="col-md-4 col-sm-6 bottommargin">
-								<div class="feature-box fbox-plain">
-									<h3>Algo que hacemos 5</h3>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem minima, facere distinctio placeat.</p>
-								</div>
-							</div>
-							<div class="col-md-4 col-sm-6 bottommargin">
-								<div class="feature-box fbox-plain">
-									<h3>Algo que hacemos 6</h3>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem minima, facere distinctio placeat.</p>
-								</div>
-							</div>
+                                                    <?php } ?>
+                                                    
 						</div>
 					</div>
 
@@ -145,10 +82,12 @@
 				</div>
 
 				<div id="portfolio" class="portfolio-full clearfix">
+                                    
+                                    <?php for($a=1;$a<=4;$a++){?>
 
 					<article class="portfolio-item pf-media pf-icons">
 						<div class="portfolio-image">
-							<img src="uploads/800x600-maquinas-1.jpg" alt="">
+                                                    <img src="<?php echo public_path("uploads/800x600-maquinas-1.jpg");?>" alt="">
 							<div class="portfolio-overlay">
 								<a href="#" class="center-icon"><i class="icon-line-ellipsis"></i></a>
 							</div>
@@ -158,49 +97,12 @@
 							<span>Modelo de máquina</span>
 						</div>
 					</article>
-
-					<article class="portfolio-item pf-illustrations">
-						<div class="portfolio-image">
-							<img src="uploads/800x600-maquinas-2.jpg" alt="">
-							<div class="portfolio-overlay">
-								<a href="#" class="center-icon"><i class="icon-line-ellipsis"></i></a>
-							</div>
-						</div>
-						<div class="portfolio-desc">
-							<h3>Tipo de máquina</h3>
-							<span>Modelo de máquina</span>
-						</div>
-					</article>
-
-					<article class="portfolio-item pf-graphics pf-uielements">
-						<div class="portfolio-image">
-							<img src="uploads/800x600-maquinas-3.jpg" alt="">
-							<div class="portfolio-overlay">
-								<a href="#" class="center-icon"><i class="icon-line-ellipsis"></i></a>
-							</div>
-						</div>
-						<div class="portfolio-desc">
-							<h3>Tipo de máquina</h3>
-							<span>Modelo de máquina</span>
-						</div>
-					</article>
-
-					<article class="portfolio-item pf-icons pf-illustrations">
-						<div class="portfolio-image">
-							<img src="uploads/800x600-maquinas-4.jpg" alt="">
-							<div class="portfolio-overlay">
-								<a href="#" class="center-icon"><i class="icon-line-ellipsis"></i></a>
-							</div>
-						</div>
-						<div class="portfolio-desc">
-							<h3>Tipo de máquina</h3>
-							<span>Modelo de máquina</span>
-						</div>
-					</article>
+                                    
+                                    <?php } ?>
 
 				</div>
 
-				<a href="contacto.html" class="button button-3d nobottomborder button-full center tright bottommargin-lg t300 font-primary" style="font-size: 26px;">
+				<a href="<?php echo url_for("contacto/index");?>" class="button button-3d nobottomborder button-full center tright bottommargin-lg t300 font-primary" style="font-size: 26px;">
 					<div class="container clearfix">
 						¿Estás interesado en alguno de nuestros servicios? <strong>Consulta aquí</strong> <i class="icon-angle-right" style="top:3px;"></i>
 					</div>
@@ -214,12 +116,9 @@
 						<p>Nos esforzamos para entregarle a nuestros clientes lo mejor. Es por esto que en Futamaq trabajamos con las mejores marcas en el rubro agrícola.</p>
 
 						<ul class="clients-grid grid-3 nobottommargin clearfix">
-							<li style="padding: 20px;"><img src="uploads/320x240-brand-logos.png" alt="Nombre cliente"></li>
-							<li style="padding: 20px;"><img src="uploads/320x240-brand-logos.png" alt="Nombre cliente"></li>
-							<li style="padding: 20px;"><img src="uploads/320x240-brand-logos.png" alt="Nombre cliente"></li>
-							<li style="padding: 20px;"><img src="uploads/320x240-brand-logos.png" alt="Nombre cliente"></li>
-							<li style="padding: 20px;"><img src="uploads/320x240-brand-logos.png" alt="Nombre cliente"></li>
-							<li style="padding: 20px;"><img src="uploads/320x240-brand-logos.png" alt="Nombre cliente"></li>
+                                                    <?php for($a=1;$a<=6;$a++){?>
+							<li style="padding: 20px;"><img src="<?php echo public_path("uploads/320x240-brand-logos.png");?>" alt="Nombre cliente"></li>
+                                                    <?php } ?>
 						</ul>
 					</div>
 
@@ -227,14 +126,16 @@
 						<h4>Noticias</h4>
 
 						<div id="oc-posts" class="owl-carousel posts-carousel">
+                                                    
+                                                    <?php for($a=1;$a<=6;$a++){?>
 
 							<div class="oc-item">
 								<div class="ipost clearfix">
 									<div class="entry-image">
-										<a href="#" data-lightbox="image"><img class="image_fade" src="uploads/500x280-noticias-home-1.jpg" alt=""></a>
+										<a href="#" data-lightbox="image"><img class="image_fade" src="<?php echo public_path("uploads/500x280-noticias-home-1.jpg");?>" alt=""></a>
 									</div>
 									<div class="entry-title">
-										<h3><a href="#">Este es el título de la noticia</a></h3>
+										<h3><a href="#">Este es el título de la noticia <?php echo $a;?></a></h3>
 									</div>
 									<ul class="entry-meta clearfix">
 										<li>10 julio 2015</li>
@@ -244,40 +145,8 @@
 									</div>
 								</div>
 							</div>
-
-							<div class="oc-item">
-								<div class="ipost clearfix">
-									<div class="entry-image">
-										<a href="#" data-lightbox="image"><img class="image_fade" src="uploads/500x280-noticias-home-2.jpg" alt=""></a>
-									</div>
-									<div class="entry-title">
-										<h3><a href="#">Este es el título de la noticia</a></h3>
-									</div>
-									<ul class="entry-meta clearfix">
-										<li>10 julio 2015</li>
-									</ul>
-									<div class="entry-content">
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
-									</div>
-								</div>
-							</div>
-
-							<div class="oc-item">
-								<div class="ipost clearfix">
-									<div class="entry-image">
-										<a href="#" data-lightbox="image"><img class="image_fade" src="uploads/500x280-noticias-home-3.jpg" alt=""></a>
-									</div>
-									<div class="entry-title">
-										<h3><a href="#">Este es el título de la noticia</a></h3>
-									</div>
-									<ul class="entry-meta clearfix">
-										<li>10 julio 2015</li>
-									</ul>
-									<div class="entry-content">
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
-									</div>
-								</div>
-							</div>
+                                                    
+                                                    <?php } ?>
 
 						</div>
 
@@ -316,7 +185,7 @@
 
 						<div class="row">
 							<div class="col-md-7">
-								<img src="uploads/800x500-home.jpg" alt="Bottom Trust">
+								<img src="<?php echo public_path("uploads/800x500-home.jpg");?>" alt="Bottom Trust">
 							</div>
 							<div class="col-md-5 topmargin-sm">
 								<div class="heading-block nobottomborder">
