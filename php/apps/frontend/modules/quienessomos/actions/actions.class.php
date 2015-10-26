a259a277532169a7a4f120505ae5c2dbdbb2b8b2
Jaime Vasquez <rodrigoxv@gmail.com>
2015-10-26 15:27:23 -0300

    quienes somos

--- php/apps/frontend/modules/quienessomos/actions/actions.class.php
+++ php/apps/frontend/modules/quienessomos/actions/actions.class.php
@@ -17,6 +17,6 @@ class quienessomosActions extends sfActions
   */
   public function executeIndex(sfWebRequest $request)
   {
-    $this->forward('default', 'module');
+      
   }
 }
---
+++ php/apps/frontend/modules/quienessomos/templates/indexSuccess.php
@@ -0,0 +1,41 @@
+                <!-- Page Title
+		============================================= -->
+		<section id="page-title" class="page-title-parallax page-title-dark" style="background-image: url('<?php echo public_path("uploads/somos-header-bg.jpg");?>'); padding: 120px 0;" data-stellar-background-ratio="0.3">
+
+			<div class="container clearfix">
+				<h1>Quiénes somos</h1>
+				<span>Tecnología a su alcance</span>
+				<ol class="breadcrumb">
+					<li><a href="<?php echo url_for("home/index");?>">Home</a></li>
+					<li class="active">Quiénes somos</li>
+				</ol>
+			</div>
+
+		</section><!-- #page-title end -->
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+
+			<div class="content-wrap">
+
+				<div class="container clearfix">
+
+					<!-- Sidebar
+                    ============================================= -->
+                    <div class="sidebar nomargin clearfix">
+                        <div class="sidebar-widgets-wrap">
+                            <blockquote class="bottommargin">
+								<p>Human rights healthcare immunize; advancement grantees. Medical supplies; meaningful, truth technology catalytic effect. Promising development capacity building international enable poverty.</p>
+							</blockquote>
+						</div>
+
+                    </div><!-- .sidebar end -->
+
+					<div class="postcontent col_last">Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus. Aenean lacinia bibendum nulla sed consectetur. Cras mattis consectetur purus sit amet fermentum. Donec id elit non mi porta gravida at eget metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id ligula porta felis euismod semper.</div>
+
+				</div>
+
+			</div>
+
+		</section><!-- #content end -->
