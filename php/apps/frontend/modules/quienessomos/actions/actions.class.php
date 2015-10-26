8d85435e5bd4fcedbbd078757cb05355ab85c2e3
Jaime Vasquez <rodrigoxv@gmail.com>
2015-10-26 13:40:22 -0300

    modulos front

---
+++ php/apps/frontend/modules/quienessomos/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * quienessomos actions.
+ *
+ * @package    Futumaq
+ * @subpackage quienessomos
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class quienessomosActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
